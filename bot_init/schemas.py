376696ec1b82cc2e213b3a82f315c987b11388c9
Almaz <a.ilaletdinov@yandex.ru>
2020-11-29 06:44:37 +0300

    Move answer class and create 'send' and 'edit' methdos for it

--- bot_init/schemas.py
+++ bot_init/schemas.py
@@ -3,12 +3,6 @@ from typing import NamedTuple
 from bot_init.markup import InlineKeyboard, Keyboard  #, get_default_keyboard
 
 
-class Answer(NamedTuple):
-    text: str = None
-    keyboard: Keyboard or InlineKeyboard = None # = get_default_keyboard()
-    tg_audio_id: str = None
-
-
 SUBSCRIBER_ACTIONS = (
     ('subscribed', 'подписался'),
     ('unsubscribed',  'отписался'),
---
+++ bot_init/services/answer.py
@@ -0,0 +1,64 @@
+from loguru import logger
+
+from bot_init.markup import get_default_keyboard
+from bot_init.utils import get_tbot_instance
+
+log = logger.bind(task="write_out_data")
+app_log = logger.bind(task="app")
+tbot = get_tbot_instance()
+
+
+# TODO жирные методы
+class Answer:
+    text = str
+    keyboard = get_default_keyboard()
+    chat_id: int
+
+    def __init__(self, text, chat_id: int = None, keyboard=None, message_key=None):
+        self.text = text
+        if keyboard is not None:
+            self.keyboard = keyboard
+        if chat_id is not None:
+            self.chat_id = chat_id
+        self.message_key = message_key
+
+    def send(self, chat_id: int = None):
+        from bot_init.utils import save_message
+        if chat_id is None:
+            chat_id = self.chat_id
+        if chat_id is None:
+            raise Exception("Не передан идентификатор чата")
+        try:
+            if self.keyboard:
+                message = tbot.send_message(chat_id=chat_id, text=self.text, reply_markup=self.keyboard, parse_mode="HTML")
+                save_message(message, message_key=self.message_key)
+                log.info(str(message))
+                return
+            message = tbot.send_message(chat_id=chat_id, text=self.text, parse_mode="HTML")
+            log.info(str(message))
+            save_message(message, message_key=self.message_key)
+        except Exception as e:
+            app_log.error(e)
+
+    def edit(self, message_id: int, chat_id: int = None):
+        from bot_init.utils import save_message
+        if chat_id is None:
+            chat_id = self.chat_id
+        if chat_id is None:
+            raise Exception("Не передан идентификатор чата")
+        try:
+            if self.keyboard:
+                message = tbot.edit_message_text(
+                    chat_id=chat_id,
+                    message_id=message_id,
+                    text=self.text,
+                    reply_markup=self.keyboard,
+                    parse_mode="HTML"
+                )
+                log.info(str(message))
+                return
+            message = tbot.send_message(chat_id=chat_id, text=self.text, parse_mode="HTML")
+            log.info(str(message))
+            save_message(message)
+        except Exception as e:
+            app_log.error(e)
